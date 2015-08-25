f7d3e5a10534bd4a21f2db78441b2dc8306c0d9c
Krasimir <kkrassi1984@abv.bg>
2015-08-25 14:54:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<img src="http://myjeeva.com/content/blog/php-fpm-nginx.png"/>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/2000px-Python-logo-notext.svg.png"/>
